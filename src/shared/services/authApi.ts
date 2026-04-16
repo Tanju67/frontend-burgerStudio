37847ac01eeeac9cff34334c99a8e30ba93de2c9
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-17 01:08:57 +0200

    feat: setup Redux Store and RTK Query auth service

---
+++ src/shared/services/authApi.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:10000/api/v1",
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["User", "Product", "Menu", "Order"],
+  endpoints: (builder) => ({}),
+});
+
+// export const {} = authApi;
