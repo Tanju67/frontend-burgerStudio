d6e534409fcbbf6416e30e7a35a91ba358b9ec3e
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-17 02:07:02 +0200

    feat: implement register mutation in authApi

--- src/shared/services/authApi.ts
+++ src/shared/services/authApi.ts
@@ -1,7 +1,18 @@
+import type { RegisterFormData } from "../schemas/authSchemas";
+import type { RegisterResponse } from "../types/types";
 import { baseApi } from "./baseApi";
 
 export const authApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    register: builder.mutation<RegisterResponse, RegisterFormData>({
+      query: (userData) => ({
+        url: "/auth/register",
+        method: "POST",
+        body: userData,
+      }),
+      invalidatesTags: ["User"],
+    }),
+  }),
 });
 
-export const {} = authApi;
+export const { useRegisterMutation } = authApi;
