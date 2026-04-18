9defbb206f2b8811d73020eaafa262f6809001f4
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-18 02:50:44 +0200

    feat(api): add create, update, delete endpoints for Menu and secure getMenus with Zod

--- src/shared/services/menuApi.ts
+++ src/shared/services/menuApi.ts
@@ -1,4 +1,9 @@
-import { menuSchema, type Menu } from "../schemas/menuSchemas";
+import {
+  menuSchema,
+  type AddMenu,
+  type Menu,
+  type UpdateMenuForm,
+} from "../schemas/menuSchemas";
 import { baseApi } from "./baseApi";
 
 export const menuApi = baseApi.injectEndpoints({
@@ -20,7 +25,41 @@ export const menuApi = baseApi.injectEndpoints({
       },
       providesTags: ["Menu"],
     }),
+
+    createMenu: builder.mutation<AddMenu, FormData>({
+      query: (formData) => ({
+        url: "/menu",
+        method: "POST",
+        body: formData,
+      }),
+      invalidatesTags: ["Menu"],
+    }),
+
+    updateMenu: builder.mutation<
+      UpdateMenuForm,
+      { id: string; data: FormData }
+    >({
+      query: ({ id, data: formData }) => ({
+        url: `/menu/${id}`,
+        method: "PATCH",
+        body: formData,
+      }),
+      invalidatesTags: ["Menu"],
+    }),
+
+    deleteMenu: builder.mutation({
+      query: (id) => ({
+        url: `/menu/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Menu"],
+    }),
   }),
 });
 
-export const { useGetMenusQuery } = menuApi;
+export const {
+  useGetMenusQuery,
+  useCreateMenuMutation,
+  useUpdateMenuMutation,
+  useDeleteMenuMutation,
+} = menuApi;
