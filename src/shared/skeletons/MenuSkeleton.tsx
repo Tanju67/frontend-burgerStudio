9691925085fee5f499d744f2b9d3065fd986fe43
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-18 02:19:49 +0200

    create MenuSkeleton component

---
+++ src/shared/skeletons/MenuSkeleton.tsx
@@ -0,0 +1,20 @@
+function MenuSkeleton() {
+  const skeletons = Array.from({ length: 6 });
+
+  return (
+    <div className="container-box mt-28 grid grid-cols-1 gap-4 py-4 md:mt-0 md:grid-cols-2">
+      {skeletons.map((_, index) => (
+        <div
+          key={index}
+          className="relative h-70 w-full animate-pulse overflow-hidden rounded-2xl bg-gray-200 md:h-80 lg:h-100 dark:bg-neutral-800"
+        >
+          <div className="h-full w-full bg-gray-300 dark:bg-neutral-700" />
+
+          <div className="absolute bottom-4 left-4 h-8 w-1/3 rounded bg-gray-400 opacity-50 dark:bg-neutral-600" />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MenuSkeleton;
